3b26335a76a52487bb13b2e988db981b590b731a
Rafael Dantas <rafael.dantas.br@gmail.com>
2018-08-30 16:52:49 -0300

    width fixes

--- src/templates/Footer.js
+++ src/templates/Footer.js
@@ -3,21 +3,23 @@ import React, { Component } from 'react';
 const Footer = () => {
 	return (
 		<footer className="footer montserrat">
-			<img className="footer__logo-icon" src="assets/img/logo.png" alt="Logo da companhia Stop and Shop" />
+			<div className="footer__wrapper">
+				<img className="footer__logo-icon" src="assets/img/logo.png" alt="Logo da companhia Stop and Shop" />
 
-			<ul className="footer__social-icon-list">
-				<li> 
-					<a href="https://www.facebook.com" target="_blank" rel="noopener"><img className="footer__social-icon" src="assets/img/facebook.svg" alt="Logo do Facebook - Link para a página"/></a>
-				</li>
+				<ul className="footer__social-icon-list">
+					<li> 
+						<a href="https://www.facebook.com" target="_blank" rel="noopener"><img className="footer__social-icon" src="assets/img/facebook.svg" alt="Logo do Facebook - Link para a página"/></a>
+					</li>
 
-				<li>
-					<a href="https://www.twitter.com" target="_blank" rel="noopener"><img className="footer__social-icon" src="assets/img/twitter.svg" alt="Logo do twitter - Link para a conta"/></a>
-				</li>
-				
-				<li>
-					<a href="https://www.instagram.com" target="_blank" rel="noopener"><img className="footer__social-icon" src="assets/img/instagram.svg" alt="Logo do Instagram - Link para a conta"/></a>
-				</li>
-			</ul>
+					<li>
+						<a href="https://www.twitter.com" target="_blank" rel="noopener"><img className="footer__social-icon" src="assets/img/twitter.svg" alt="Logo do twitter - Link para a conta"/></a>
+					</li>
+					
+					<li>
+						<a href="https://www.instagram.com" target="_blank" rel="noopener"><img className="footer__social-icon" src="assets/img/instagram.svg" alt="Logo do Instagram - Link para a conta"/></a>
+					</li>
+				</ul>
+			</div>
 		</footer>
 	)
 }
--- src/templates/Menu.js
+++ src/templates/Menu.js
@@ -5,10 +5,12 @@ class Menu extends Component {
 	render() {
 		return (
 			<nav className="menu">
-				<Link to="/">
-					<img className="menu__logo-icon" src="assets/img/logo.png" alt="Logo da companhia Stop and Shop" />
-				</Link>
-				<img className="menu__checkout-icon" src="assets/img/checkout.svg" alt="Icone do Carrinho de compras" />
+				<div className="menu__wrapper">
+					<Link to="/">
+						<img className="menu__logo-icon" src="assets/img/logo.png" alt="Logo da companhia Stop and Shop" />
+					</Link>
+					<img className="menu__checkout-icon" src="assets/img/checkout.svg" alt="Icone do Carrinho de compras" />
+				</div>
 			</nav>
 		)
 	}
